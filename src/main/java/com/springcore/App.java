45a17681e28716e8588d0998e5a28f08c76aee6b
KAMESHWARSAH <javacore54@gmail.com>
2024-04-20 02:43:07 +0530

    Second commit

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -14,6 +14,8 @@ public class App
         System.out.println( "Hello World!" );
      ApplicationContext context=   new ClassPathXmlApplicationContext("config.xml");
      Student mystudent = (Student) context.getBean("mystudent");
-     System.out.print(mystudent);
+     Student mystudent2 = (Student) context.getBean("mystudent2");
+     System.out.println(mystudent);
+     System.out.println(mystudent2);
     }
 }
